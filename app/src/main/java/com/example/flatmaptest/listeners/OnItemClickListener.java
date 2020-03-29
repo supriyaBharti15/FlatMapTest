e4a5bb3d34c143f859182a01c6519066c8ea2134
supriyaBharti15 <supriya.simi@gmail.com>
2020-03-29 17:23:14 +0530

    set click on row item

---
+++ app/src/main/java/com/example/flatmaptest/listeners/OnItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.flatmaptest.listeners;
+
+import com.example.flatmaptest.model.Data;
+
+public interface OnItemClickListener {
+    public void onItemClicked(Data data);
+}
--- app/src/main/java/com/example/flatmaptest/views/adapter/MainActivityAdapter.java
+++ app/src/main/java/com/example/flatmaptest/views/adapter/MainActivityAdapter.java
@@ -4,17 +4,21 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.flatmaptest.BR;
 import com.example.flatmaptest.R;
 import com.example.flatmaptest.databinding.DataItemBinding;
+import com.example.flatmaptest.listeners.OnItemClickListener;
 import com.example.flatmaptest.model.Data;
 import com.example.flatmaptest.model.MasterDataModel;
 
-public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapter.MyViewHolderClass> {
+public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapter.MyViewHolderClass> implements OnItemClickListener {
     private MasterDataModel masterDataModel;
     private Context context;
 
@@ -34,6 +38,7 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
     public void onBindViewHolder(@NonNull MyViewHolderClass holder, int position) {
         Data data = masterDataModel.getAllUserResponse().getData().get(position);
         holder.dataItemBinding.setAllUserData(data);
+        holder.dataItemBinding.setOnItemClicked(this);
     }
 
     @Override
@@ -41,12 +46,22 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
         return masterDataModel.getAllUserResponse().getData().size();
     }
 
+    @Override
+    public void onItemClicked(Data data) {
+        Toast.makeText(context,data.getFirstName(),Toast.LENGTH_LONG).show();
+    }
+
+
     public class MyViewHolderClass extends RecyclerView.ViewHolder {
         DataItemBinding dataItemBinding;
 
         public MyViewHolderClass(@NonNull DataItemBinding binding) {
             super(binding.getRoot());
             this.dataItemBinding = binding;
+        }
+        public void bind(Object obj){
+            dataItemBinding.setVariable(com.example.flatmaptest.BR.allUserData,obj);
+            dataItemBinding.executePendingBindings();
         }
     }
 }
